e3e193b7eeb2d7fbf298b233f356896d97c8df25
Diane E. Phillips <diannypanny@Dianes-MacBook-Pro.local>
2015-07-01 19:09:05 -0700

    adding more steps

--- Final_Project.py
+++ Final_Project.py
@@ -17,14 +17,27 @@ class BucketList(object):
 		self.my_list.append(item)
 
 	def print_bucket(self):
-		print "DO OR DIE"
+		print "NOW GO MAKE IT HAPPEN!"
 		for item in self.my_list:
 			print item
 
-my_bucket_list = BucketList() # create a new instance of a to do list
-my_bucket_list.add("Hike up Mt. Kili") #call the add method on my_to_do_list
-my_bucket_list.add("Explore the Amazons in Brazil") #call the add method on my_to_do_list
+my_bucket_list = BucketList() # create a new instance of bucket list
+#get input from user 
+while True:
+	user_input = raw_input("What's the most dangerous thing on your bucket list? ")
+	#user_input = raw_input("What would you do if you had unlimited money? ")
+	#user_input = raw_input("What would you do if you had unlimited time? ")
+	#user_input = raw_input("What skills do you want to learn? ")
+	#user_input = raw_input("If you're all finished, write exit! ")
+	if user_input == "exit!":
+		break
+	my_bucket_list.add(user_input) #call the add method on my_to_do_list
+	#my_bucket_list.add("Explore the Amazons in Brazil") #call the add method on my_to_do_list
+
 my_bucket_list.print_bucket()
+
+#want to make it look pretty
+#
 
 
 
